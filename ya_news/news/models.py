aea79df88440751295e5639dda5ee9030bb0d41a
Forget-me-not-crossyroad <milleralex629@gmail.com>
2023-08-26 20:43:49 +0300

    Code improvements according to PEP8

--- ya_news/news/models.py
+++ ya_news/news/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from django.conf import settings
 from django.db import models
--- ya_news/pytest_trial/test_engine.py
+++ ya_news/pytest_trial/test_engine.py
@@ -1,4 +1,3 @@
-import pytest
 from engine_class import Engine
 
 
